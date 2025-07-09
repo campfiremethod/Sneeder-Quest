82d7b57d3ab380c746d1b422dbc3510b5ca2e493
unknown <biscuit.ninja@gmail.com>
2025-07-09 10:27:24 -0500

    seperated progressbar function. removed unused files

--- main.js
+++ main.js
@@ -432,52 +432,7 @@ function Put(list, key, value) {
   }
 }
 
-function ProgressBar(id, tmpl) {
-  this.id = id;
-  this.bar = $("#" + id + " > .bar");
-  this.tmpl = tmpl;
 
-  this.Max = function () {
-    return game[this.id].max;
-  };
-  this.Position = function () {
-    return game[this.id].position;
-  };
-
-  this.reset = function (newmax, newposition) {
-    game[this.id].max = newmax;
-    this.reposition(newposition || 0);
-  };
-
-  this.reposition = function (newpos) {
-    game[this.id].position = Min(newpos, this.Max());
-
-    // Recompute hint
-    game[this.id].percent = (100 * this.Position()).div(this.Max());
-    game[this.id].remaining = Math.floor(this.Max() - this.Position());
-    game[this.id].time = RoughTime(this.Max() - this.Position());
-    game[this.id].hint = template(this.tmpl, game[this.id]);
-
-    // Update UI
-    if (this.bar) {
-      var p = this.Max() ? (100 * this.Position()) / this.Max() : 0;
-      this.bar.css("width", p + "%");
-      this.bar.parent().find(".hint").text(game[this.id].hint);
-    }
-  };
-
-  this.increment = function (inc) {
-    this.reposition(this.Position() + inc);
-  };
-
-  this.done = function () {
-    return this.Position() >= this.Max();
-  };
-
-  this.load = function (game) {
-    this.reposition(this.Position());
-  };
-}
 
 function Key(tr) {
   return $(tr).children().first().text();
